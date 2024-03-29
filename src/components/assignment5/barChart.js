7ee37817ade769ae53f3e4a3cb45534b1530343c
HelennnZ <qz2086@nyu.edu>
2024-03-30 07:20:49 +0800

    modified:   src/components/assignment5/airportMap.js 	modified:   src/components/assignment5/barChart.js 	modified:   src/components/assignment5/routes.js 	modified:   src/pages/assignment5_student.js

--- src/components/assignment5/barChart.js
+++ src/components/assignment5/barChart.js
@@ -4,7 +4,7 @@ import { XAxis, YAxis } from "./axes";
 
 
 export function BarChart (props) {
-    const {offsetX, offsetY, data, height, width, selectedAirlineID, setSelectedAirlineID} = props;
+    const {offsetX, offsetY, height, width, data, selectedAirline, setSelectedAirline} = props;
     // Task 1: TODO
     
     // 1. find the maximum of the Count attribute in the data
@@ -20,16 +20,6 @@ export function BarChart (props) {
         .range([0, height])
         .padding(0.2);
 
-    const bars = data.map(d => (
-        <rect key={d.AirlineID} 
-                y={yScale(d.AirlineName)}
-                width={xScale(d.Count)}
-                height={yScale.bandwidth()}
-                stroke = "black"
-                fill="#2a5599" />
-    ));
-
-    
     // Task 3. TODO
     // 1. define an arrow function color; it takes a data item, d, as input. 
     // If d.AirlineID is equal to the selectedAirlineID, it returns "#992a5b"; 
@@ -40,11 +30,34 @@ export function BarChart (props) {
     // 4. adding properties, onMouseOver and onMouseOut, to the <rect> tags.
     // Note: the function of the onMouseOver properties should be an arrow function 
     // that wraps the onMouseOver you defined since it takes d as input.
-    
+
+    const color = d => d.AirlineID === selectedAirline ? "#992a5b" : "#2a5599";
+
+    // Define onMouseOver handler
+    const onMouseOver = d => {
+        setSelectedAirline(d.AirlineID);
+    };
+
+    // Define onMouseOut handler
+    const onMouseOut = () => {
+        setSelectedAirline(null);
+    };
+
+    // Generate bars using the color function and event handlers
+    const bars = data.map(d => (
+        <rect key={d.AirlineID} 
+                y={yScale(d.AirlineName)}
+                width={xScale(d.Count)}
+                height={yScale.bandwidth()}
+                stroke="black"
+                fill={color(d)} // Use the color function here
+                onMouseOver={() => onMouseOver(d)} // Set onMouseOver handler
+                onMouseOut={onMouseOut} // Set onMouseOut handler
+        />
+    ));
+
     return (
         <g transform={`translate(${offsetX}, ${offsetY})`}>
-
-{/* // 3. return the bars */}
             {bars}
             <XAxis xScale={xScale} width={width} height={height} />
             <YAxis yScale={yScale} height={height} offsetX={offsetX} />
--- src/pages/assignment5_student.js
+++ src/pages/assignment5_student.js
@@ -7,6 +7,8 @@ import { groupByAirline, groupByAirport } from "../components/assignment5/utils"
 import { AirportMap }  from "../components/assignment5/airportMap";
 import { BarChart } from "../components/assignment5/barChart";
 
+import styles from "../styles/assignment5_styles.module.css"
+
 
 const csvUrl = 'https://gist.githubusercontent.com/hogwild/9367e694e12bd2616205e4b3e91285d5/raw/9b451dd6bcc148c3553f550c92096a1a58e1e1e5/airline-routes.csv';
 const mapUrl = 'https://gist.githubusercontent.com/hogwild/26558c07f9e4e89306f864412fbdba1d/raw/5458902712c01c79f36dc28db33e345ee71487eb/countries.geo.json';
@@ -15,7 +17,7 @@ function useData(csvPath){
     const [dataAll, setData] = React.useState(null);
     React.useEffect(() => {
         csv(csvPath).then(data => {
-            data.forEach(d => {
+            data?.forEach(d => {
                 d.SourceLatitude = +d.SourceLatitude
                 d.SourceLongitude = +d.SourceLongitude
                 d.DestLatitude = +d.DestLatitude
@@ -58,13 +60,13 @@ function AirlineRoutes(){
     return <Container >
         <Row className={"justify-content-md-left"}>
             <Col lg={10} >
-                <h1>Airlines Routes</h1> 
+            <h1 className={styles.h1Style}> Airlines Routes </h1>
             </Col>
         </Row>  
         <Row className={"justify-content-md-left"}>
         <Col lg={4}>
             <h2>Airlines</h2>
-            <svg id={"barchart"} width={barchart_width} height={barchart_height}>
+            <svg className={styles.svgStyle} id={"barchart"} width={barchart_width} height={barchart_height}>
                 <BarChart offsetX={barchart_margin.left} offsetY={barchart_margin.top} 
                     height={barchart_inner_height} width={barchart_inner_width} data={airlines}
                     selectedAirline={selectedAirline} setSelectedAirline={setSelectedAirline}
@@ -73,7 +75,7 @@ function AirlineRoutes(){
         </Col>
         <Col lg={4} >
             <h2>Airports</h2>
-            <svg id={"map"} width={map_width} height={map_height}>
+            <svg className={styles.svgStyle} id={"map"} width={map_width} height={map_height}>
                 <AirportMap width={map_width} height={map_height} 
                     countries={map} airports={airports} routes={routes}
                     selectedAirline={selectedAirline}
